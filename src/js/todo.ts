463057e08772406b651839be18a70f99c1786bc6
加藤真優子 <>
2025-04-04 21:19:59 +0900

    TODOアプリの実装②(削除ボタン・絞り込み機能の実装)

--- src/js/todo.ts
+++ src/js/todo.ts
@@ -4,6 +4,7 @@ import { createElement, getElementById, getInputElementById } from "./utils/dom"
  * TODOの型定義
  */
 export type Todo = {
+  id: number;
   name: string;
   person: string;
   deadline: string;
@@ -15,6 +16,7 @@ export type Todo = {
  */
 
 export const getNewTodo = (): Todo => ({
+  id: Date.now(),
   name: getInputElementById("new-todo-name").value,
   person: getInputElementById("new-person").value,
   deadline: getInputElementById("new-deadline").value,
@@ -24,15 +26,35 @@ export const getNewTodo = (): Todo => ({
  * DOMにTODO一覧を表示する
  */
 
-export const appendTodoList = (todoList: Todo[]) => {
-  todoList.forEach((todo) => {
+export const appendTodoList = (_todoList: Todo[], _filterWord: string, deleteTodo: (id: number) => void) => {
+  _todoList.
+  filter(
+    (todo) =>
+      todo.name.includes(_filterWord) ||
+      todo.person.includes(_filterWord)
+  )
+  .forEach((todo) => {
     const nameTd = createElement("td", todo.name);
     const personTd = createElement("td", todo.person);
     const deadlineTd = createElement("td", todo.deadline);
+
+    // 削除ボタン
+    const deleteButton = createElement("button", "削除");
+    deleteButton.addEventListener("click", () => deleteTodo(todo.id));
+    // deleteButton.addEventListener("click", () => {
+    //   _todoList = _todoList.filter((_todo) => _todo.id !== todo.id);
+    //   removeTodoListElement();
+    //   appendTodoList(_todoList);
+    // });
+    const deleteButtonTd = createElement("td");
+    deleteButtonTd.appendChild(deleteButton);
+
+
     const tr = createElement("tr");
     tr.appendChild(nameTd);
     tr.appendChild(personTd);
     tr.appendChild(deadlineTd);
+    tr.appendChild(deleteButtonTd);
     const tBody = getElementById("todo-list");
     tBody.appendChild(tr);
   }
